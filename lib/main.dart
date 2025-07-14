fe61130f9356cb4e2446b9529fd4f65c228be636
pangping <fanbueto@gmail.com>
2025-07-14 16:54:07 +0700

    งานเขียนuiตามรูบนโทรศัพท์ phone-14.png

--- lib/main.dart
+++ lib/main.dart
@@ -1,172 +1,175 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MainApp());
-}
+void main() => runApp(const MyApp());
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: const Color.fromARGB(255, 12, 106, 194),
-          title: const Text('Picture Text Button'),
+      theme: ThemeData(
+        primaryColor: AppTheme.primaryGreen,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: AppTheme.primaryGreen,
         ),
-        body: Center(
-          child: ListView(
-            shrinkWrap: true,
-            physics: const AlwaysScrollableScrollPhysics(), // เปลี่ยนเพื่อให้เลื่อนได้
-            children: [
-              // Box 1
-              Container(
-                color: const Color.fromARGB(255, 194, 12, 12),
-                width: 350,
-                height: 200,
-                margin: const EdgeInsets.only(bottom: 16),
-                padding: const EdgeInsets.all(8),
-                child: Row(
-                  children: [
-                    Image.asset(
-                      'asset/images/pic.jpg',
-                      width: 100,
-                      height: 100,
-                      fit: BoxFit.cover,
-                    ),
-                          const Text(
-                            'Box 1 Title',
-                            style: TextStyle(color: Colors.white, fontSize: 18),
-                            textAlign: TextAlign.left,
-                          ),
-                    const SizedBox(width: 12),
-                    Expanded(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          const SizedBox(height: 8),
-                          ElevatedButton(
-                            onPressed: () {},
-                            child: const Text('Click Me'),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              // Box 2
-              Container(
-                color: const Color.fromARGB(255, 12, 106, 194),
-                width: 350,
-                height: 200,
-                margin: const EdgeInsets.only(bottom: 16),
-                padding: const EdgeInsets.all(8),
-                child: Row(
-                  children: [
-                    Image.asset(
-                      'asset/images/pic.jpg',
-                      width: 100,
-                      height: 100,
-                      fit: BoxFit.cover,
-                    ),
-                    const SizedBox(width: 12),
-                    Expanded(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          const Text(
-                            'Box 2 Title',
-                            style: TextStyle(color: Colors.white, fontSize: 18),
-                            textAlign: TextAlign.center,
-                          ),
-                          const SizedBox(height: 8),
-                          ElevatedButton(
-                            onPressed: () {},
-                            child: const Text('Click Me'),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              // Box 3
-              Container(
-                color: const Color.fromARGB(255, 20, 150, 50),
-                width: 350,
-                height: 200,
-                margin: const EdgeInsets.only(bottom: 16),
-                padding: const EdgeInsets.all(8),
-                child: Row(
-                  children: [
-                    Image.asset(
-                      'asset/images/pic.jpg',
-                      width: 100,
-                      height: 100,
-                      fit: BoxFit.cover,
-                    ),
-                    const SizedBox(width: 12),
-                    Expanded(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          const Text(
-                            'Box 3 Title',
-                            style: TextStyle(color: Colors.white, fontSize: 18),
-                            textAlign: TextAlign.center,
-                          ),
-                          const SizedBox(height: 8),
-                          ElevatedButton(
-                            onPressed: () {},
-                            child: const Text('Click Me'),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              // Box 4 (ใหม่ เพิ่มมา)
-              Container(
-                color: const Color.fromARGB(255, 120, 60, 180),
-                width: 350,
-                height: 200,
-                padding: const EdgeInsets.all(8),
-                child: Row(
-                  children: [
-                    Image.asset(
-                      'asset/images/pic.jpg',
-                      width: 100,
-                      height: 100,
-                      fit: BoxFit.cover,
-                    ),
-                    const SizedBox(width: 12),
-                    Expanded(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          const Text(
-                            'Box 4 Title',
-                            style: TextStyle(color: Colors.white, fontSize: 18),
-                            textAlign: TextAlign.center,
-                          ),
-                          const SizedBox(height: 8),
-                          ElevatedButton(
-                            onPressed: () {},
-                            child: const Text('Click Me'),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
+        bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+          selectedItemColor: AppTheme.primaryGreen,
+          unselectedItemColor: Colors.grey,
         ),
+      ),
+      home: const WelcomePage(),
+    );
+  }
+}
+
+class WelcomePage extends StatefulWidget {
+  const WelcomePage({super.key});
+
+  @override
+  State<WelcomePage> createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  int _currentIndex = 0;
+
+  @override
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("Welcome Page"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Align(
+              alignment: Alignment.center,
+              child: const Text(
+                "This is a short top-level description.",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16, // เพิ่มขนาดตามต้องการ
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+            // ปุ่มอยู่ด้านบนกล่องรูป
+            Row(
+              children: [
+                Expanded(
+                  child: SizedBox(
+                    height: 48,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.orange,
+                      ),
+                      onPressed: () {},
+                      child: const Text("Button 1"),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 16),
+                Expanded(
+                  child: SizedBox(
+                    height: 48,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color.fromARGB(255, 217, 233, 72),
+                      ),
+                      onPressed: () {},
+                      child: const Text("Button 2"),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 10),
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      margin: const EdgeInsets.all(8),
+                      padding: const EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                        color: Colors.blue.shade50,
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: Column(
+                        children: [     
+                          Expanded(
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(12), // กำหนดความโค้ง
+                              child: Image.network(
+                                "https://i.pinimg.com/564x/26/df/cf/26dfcf8a37e9bf985348e7746e9a29e6.jpg",
+                                fit: BoxFit.cover,
+                                width: double.infinity,
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 8),
+                          const Text("First Image"),
+                          const Text("Sample description here"),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      margin: const EdgeInsets.all(8),
+                      padding: const EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                        color: Colors.pink.shade50,
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: Column(
+                        children: [
+                          Expanded(
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(12), // กำหนดความโค้ง
+                              child: Image.network(
+                                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVZ9naSmiAnQyibIkMtwyQf70eAHWzeTjHEQ&s",
+                                fit: BoxFit.cover,
+                                width: double.infinity,
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 8),
+                          const Text("Second Image"),
+                          const Text("Another short text"),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        selectedItemColor: AppTheme.primaryGreen,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.info), label: "About"),
+          BottomNavigationBarItem(icon: Icon(Icons.settings), label: "Setting"),
+        ],
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
       ),
     );
   }
+}
+
+// คลาสการตั้งค่าสีปุ่มและแอบบาร์
+class AppTheme {
+  static const Color primaryGreen = Color(0xFF28836C);
 }
